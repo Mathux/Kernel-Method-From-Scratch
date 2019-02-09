f77dc1a8fc4035e9d8cd5e352f98f9345b4faf0b
Evrard Garcelon <evrard.garcelon@gmail.com>
2019-02-10 00:11:19 +0100

    Voting Classifier

---
+++ voting_classifier.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 10 00:07:37 2019
+
+@author: evrardgarcelon
+"""
+
+import numpy as np
+from utils import *
+
+class VotingClassifier(object) :
+    def __init__(self) :
+        pass
