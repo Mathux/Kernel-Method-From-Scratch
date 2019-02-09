441db8723f3146ffb77bc34963df558c81a7b8e2
Evrard Garcelon <evrard.garcelon@gmail.com>
2019-02-09 23:31:35 +0100

    KernelKNN

--- kernel_knn.py
+++ kernel_knn.py
@@ -5,8 +5,78 @@ Created on Sat Feb  9 20:56:13 2019
 
 @author: evrardgarcelon
 """
+import numpy as np
+from utils import *
+import heapq as hq
 
 class KernelKNN(object) :
     
-    def __init__(self, n_neighbors = 5, kernel = 'linear', gamma = 1, dim = 1, offset = 0, ) :
-        pass
+    def __init__(self, n_neighbors = 5, kernel = 'linear', gamma = 1, dim = 1, offset = 0, scale = False) :
+        
+        self.n_neighbors = n_neighbors
+        self.kernel = get_kernel(kernel, gamma = 1, dim = 1, offset = 0)
+        self.scale = scale
+        self.kernel_type = kernel
+    
+    def fit(self,X,y) :
+        
+        self.X = X
+        self.y = self.transform_label(y)
+        self.n_samples,self.n_features = X.shape
+    
+    def predict(self,X) :
+        
+        n_test,_ = X.shape
+        predictions = np.zeros(n_test)
+        for j in range(n_test) :
+            predictions[j] = self.majority_vote(X[j])
+        return predictions
+    
+    def majority_vote(self,x) :
+        
+        nearest_neighbors = self.compute_nearest_neighbors(x)
+        majority = 0
+        for j in nearest_neighbors :
+            if self.y[j] > 0 :
+                majority +=1
+            else :
+                majority += -1
+            
+        if majority > 0 :
+            return 1
+        elif majority < 0 :
+            return 0
+        else :
+            print('lol')
+            return np.random.randint(0,2)
+            
+    def compute_nearest_neighbors(self,x) :
+        
+        distance = []
+        nearest_neighbors = []
+        for i in range(self.n_samples) :
+            hq.heappush(distance,(self.kernel(self.X[i],x),i))
+        for j in range(self.n_neighbors) : 
+            nearest_neighbors.append(hq.heappop(distance)[1])
+        print(nearest_neighbors)
+        return nearest_neighbors
+    
+    def transform_label(self,y) :
+        return 2*y - 1
+    
+    def score(self,X,y) :
+        predictions = self.predict(X)
+        return np.sum(y == predictions)/X.shape[0]
+    
+    def recall_and_precision(self,X,y) :
+        y = self.transform_label(y)
+        predictions = self.predict(X).astype('int')
+        tp = np.sum((predictions == 1)*(y == 1))
+        fn = np.sum((predictions == -1)*(y == 1))
+        fp = np.sum((predictions == 1)*(y == -1))
+        return tp/(fn+tp),tp/(fp+tp)
+            
+        
+        
+    
+            
--- temp.py
+++ temp.py
@@ -3,17 +3,29 @@ import numpy as np
 from test_data import *
 from svm import SVM
 from kernel_lr import KernelLogisticRegression
+from kernel_knn import KernelKNN
 
 plt.figure(1)
-X1,y1,X2,y2 = gen_circular_data()
+X1,y1,X2,y2 = gen_lin_separable_data()
 X = np.vstack([X1,X2])
 y = np.hstack([y1,y2])
-a = KernelLogisticRegression(kernel = 'gaussian',la = 2,gamma = 3/2)
+a = KernelKNN(kernel = 'linear',n_neighbors = 1)
 a.fit(X,y)
 
-plt.scatter(X1[:,0],X1[:,1],color = 'red',marker = 'o')
-plt.scatter(X2[:,0],X2[:,1],color = 'blue',marker = '^')
+#plt.scatter(X1[:,0],X1[:,1],color = 'red',marker = 'o')
+#plt.scatter(X2[:,0],X2[:,1],color = 'blue',marker = '^')
 
+preds = a.predict(X)
+
+for j in range(len(preds)) :
+    if preds[j] == 1 :
+        plt.scatter(X[j][0],X[j][1],color = 'magenta',marker = '+')
+    elif preds[j] == 0 :
+        plt.scatter(X[j][0],X[j][1],color = 'cyan',marker = 'x')
+    else : 
+        print('pb')
+        
+#%%
 alpha = a.alpha
 
 x,y = np.linspace(-6,6),np.linspace(-6,6)
