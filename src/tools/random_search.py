047cdca9695886da6a04673e9f67efd2aa4bde98
Evrard Garcelon <evrard.garcelon@gmail.com>
2019-03-06 02:02:39 +0100

    modle

--- src/tools/random_search.py
+++ src/tools/random_search.py
@@ -12,26 +12,26 @@ from src.tools.utils import Logger
 
 
 class RandomHyperParameterTuningPerKernel(Logger):
-    def __init__(self, dataset, clf, kernel, parameter_grid, X, y, n_sampling,
+    def __init__(self, dataset, clf, kernel, parameter_grid, n_sampling,
                  kfold=5, verbose=True):
 
         self.verbose = verbose
-        
         self.clf = clf
         self.kernel = kernel
         self.parameter_grid = parameter_grid
         self.dataset = dataset
         self.n = n_sampling
         self.kfold = kfold
-        self.kernels = parameter_grid['kernel']
+        # self.kernel_parameters = 
         # name = clf.__name__
         self.parameters = kernel.param
 
     def fit(self):
-        self.parameters = {}
+        self.kernel_parameters = {}
+        self.clf_parameters = {}
         self.scores = {}
         for param in self.kernel.param:
-            self.parameters[param] = self.parameter_grid[param].rvs(size=self.n)
+            self.kernel_parameters[param] = self.parameter_grid[param].rvs(size=self.n)
         for parameter_name in self.clf_parameters:
             self.parameters[parameter_name] = self.parameter_grid[
                 parameter_name].rvs(self.n)
@@ -44,7 +44,6 @@ class RandomHyperParameterTuningPerKernel(Logger):
             for parameter_name in self.clf_parameters:
                 temp_parameters[parameter_name] = self.parameters[
                     parameter_name][j]
-            print(temp_parameters)
             temp_clf = self.clf(**temp_parameters)
             CV = CrossValidation(self.dataset, temp_clf, k_fold=self.k_fold)
             temp_report = CV.stats
@@ -57,6 +56,7 @@ class RandomHyperParameterTuning(Logger):
                  kernels,
                  dataset,
                  n_sampling,
+                 parameter_grid,
                  criteria='accuracy',
                  kfold=5,
                  verbose=True):
@@ -112,3 +112,18 @@ class RandomHyperParameterTuning(Logger):
         best_parameters = self.parameters[best_kernel]
         best_parameters['kernel'] = best_kernel
         return best_parameters, self.accuracy[best_kernel]
+
+if __name__ == '__main__' :
+    
+    from src.data.seq import SeqData
+    from scipy.stats import uniform
+    from scipy.stats import randint
+    
+    data = SeqData(k=0, dataname="train", mat=False, small=False, verbose=True)
+    
+    parameter_grid = {'kernel' : ['spectral', 'mismatch'],
+                      'k' : randint(low = 3, high = 9),
+                      'm' : randint(low = 1, high = 3),
+                      'C' : uniform(loc = 0.1, scale = 10)
+                      }
+    
